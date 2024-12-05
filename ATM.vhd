a7163ece942b3795cb3f3a5d8f829d1035339fab
Kutukobra <nadzhiff@gmail.com>
2024-12-05 19:24:19 +0700

    SPI_MASTER: blueprint  ATM State Machine: IDLE, LOGIN

--- ATM.vhd
+++ ATM.vhd
@@ -3,10 +3,16 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity ATM is
+
+    generic (
+        ATM_ID : STD_LOGIC_VECTOR(2 downto 0) := '001';
+    )
+
     port (
+        CLK : IN STD_LOGIC;
         ATM_INSIDE : IN STD_LOGIC;
-        ACCOUNT_ID : IN STD_LOGIC_VECTOR(3 downto 0);
-        ACCOUNT_PIN : IN STD_LOGIC_VECTOR(3 downto 0)   ;
+        ACCOUNT_ID : IN STD_LOGIC_VECTOR(4 downto 0);
+        ACCOUNT_PIN : IN STD_LOGIC_VECTOR(3 downto 0);
 
         OPTION_WS : STD_LOGIC; -- 0 untuk withdraw, 1 untuk store
     );
@@ -16,8 +22,34 @@ end entity ATM;
 architecture rtl of ATM is
     type StateType is (IDLE, LOGIN, CHOOSE_OPTION, WITHDRAW, STORE, SUCCESS);
     SIGNAL STATE : StateType;
+
+    signal SPI_ACTIVE : STD_LOGIC;
+
+    signal MESSAGE_OUT : STD_LOGIC_VECTOR(15 downto 0);
+    signal MESSAGE_IN : STD_LOGIC_VECTOR(15 downto 0);
+
 begin
-    
+    TRANSITION : PROCESS(CLK)
+    begin
+        if rising_edge(CLK) and SPI_ACTIVE = '0' then
+            case STATE is
+
+                when IDLE =>
+                if ATM_INSIDE = '1' then
+                    STATE <= LOGIN
+                end if;
+
+                when LOGIN => 
+                    MESSAGE_OUT <= '11' & ATM_ID & ACCOUNT_ID & ACCOUNT_PIN & '000';
+                    SPI_ACTIVE <= '1';
+                    STATE <= CHOOSE_OPTION;
+                
+                when CHOOSE_OPTION =>
+                    
+
+            end case;
+        end if;
+    end process TRANSITION;
     
     
 end architecture rtl;
---
+++ SPI_Master.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity SPI_Master is
+    generic (
+        SLAVE_COUNT : INTEGER := 8;
+        DATA_LENGTH : INTEGER := 16;
+    )   
+
+    port (
+        SCK : IN  STD_LOGIC;
+        CSB : OUT STD_LOGIC_VECTOR(1 to SLAVE_COUNT);
+
+        SDI : IN  STD_LOGIC;
+        SDO : OUT STD_LOGIC;
+    );
+end entity SPI_Master;
