83b24afcb7656b3b7e53c32324ef0555eda3519f
Kutukobra <nadzhiff@gmail.com>
2024-12-07 19:56:57 +0700

    Server_FSM_tb: Added Server_FSM: Refactored to IDLE - EXECUTE - RESPONSE cycle ATM_FSM: Refactoring to adjust to Server_FSM

--- ATM_FSM.vhd
+++ ATM_FSM.vhd
@@ -11,7 +11,6 @@ entity ATM_FSM is
     port (
         CLK : IN STD_LOGIC;
         FSM_EN : IN STD_LOGIC;
-        RST : IN STD_LOGIC;
         
         ATM_INSIDE : IN STD_LOGIC;
         ACCOUNT_ID : IN STD_LOGIC_VECTOR(4 downto 0);
@@ -29,57 +28,27 @@ end entity ATM_FSM;
 
 
 architecture rtl of ATM_FSM is
-    type StateType is (IDLE, LOGIN, CHOOSE_OPTION, WITHDRAW, STORE, CHECK, SUCCESS);
+    type StateType is (IDLE, INPUT, SEND);
     SIGNAL STATE : StateType;
+
+    ALIAS OPCODE : STD_LOGIC_VECTOR(1 downto 0) is MESSAGE_IN(15 downto 14);
+    ALIAS ATM_ID : STD_LOGIC_VECTOR(2 downto 0) is MESSAGE_IN(13 downto 11);
+    ALIAS ACCOUNT_ID : STD_LOGIC_VECTOR(4 downto 0) is MESSAGE_IN(10 downto 6);
+    ALIAS ACCOUNT_PIN : STD_LOGIC_VECTOR(3 downto 0) is MESSAGE_IN(5 downto 2);
+    ALIAS NOMINAL : STD_LOGIC_VECTOR(7 downto 0) is MESSAGE_IN(10 downto 3);
 
 begin
     TRANSITION : PROCESS(CLK)
     begin
-
         if rising_edge(CLK) and FSM_EN = '1' then
             case STATE is
-
                 when IDLE =>
-                if ATM_INSIDE = '1' then
-                    STATE <= LOGIN;
-                end if;
-
-                when LOGIN => 
-                    MESSAGE_OUT <= "11" & ATM_ID & ACCOUNT_ID & ACCOUNT_PIN & "00";
-                    MESSAGE_SEND <= '1';
-                    STATE <= CHOOSE_OPTION;
-                
-                when CHOOSE_OPTION =>
-                    if MESSAGE_IN(15) = '0' then
-                        STATE <= IDLE;
-                    else
-                        if OPTION_WS = '0' then
-                            STATE <= WITHDRAW;
-                        else
-                            STATE <= STORE;
-                        end if;
+                    if ATM_INSIDE = '1' then
+                        STATE <= INPUT;
                     end if;
 
-                when WITHDRAW =>
-                    MESSAGE_OUT <= "01" & ATM_ID & STD_LOGIC_VECTOR(TO_UNSIGNED(NOMINAL, 8)) & "000";
-                    MESSAGE_SEND <= '1';
-                    STATE <= CHECK;
-
-                when STORE =>
-                    MESSAGE_OUT <= "10" & ATM_ID & STD_LOGIC_VECTOR(TO_UNSIGNED(NOMINAL, 8)) & "000";
-                    MESSAGE_SEND <= '1';
-                    STATE <= CHECK;
-
-                when CHECK =>
-                    if MESSAGE_IN(15) = '0' then
-                        STATE <= IDLE;
-                    else
-                        STATE <= SUCCESS;
-                    end if;
-
-                when SUCCESS => 
-                    STATE <= IDLE;
-
+                when INPUT =>
+                    
             end case;
         end if;
     end process TRANSITION;
